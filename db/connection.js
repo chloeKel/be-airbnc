821e478a33c9d74a83ce72d0476a783c8cffcb1e
legshog <chloekelleher@hotmail.co.uk>
2025-01-04 14:53:05 +0000

    troubleshooting

--- db/connection.js
+++ db/connection.js
@@ -1,24 +1,9 @@
-// const { Pool } = require("pg");
-
-// const path = `${__dirname}/../.env.test`;
-
-// require("dotenv").config({ path });
-
-// if (!process.env.PGDATABASE) {
-//   throw new Error("PGDATABASE not set");
-// }
-
-// const db = new Pool();
-
-// module.exports = db;
-
 const { Pool } = require("pg");
 
-const path = `${__dirname}/../.env.test`;
-//const ENV = process.env.NODE_ENV;
-// const path = `${__dirname}/../.env.${ENV}`;
-
 const ENV = process.env.NODE_ENV || "development";
+
+require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
@@ -30,12 +15,10 @@ if (ENV === "production") {
   config.max = 2;
 }
 
-require("dotenv").config({ path });
-
 if (!process.env.PGDATABASE) {
   throw new Error("PGDATABASE not set");
 }
 
-const db = new Pool();
+const db = new Pool(config);
 
 module.exports = db;
