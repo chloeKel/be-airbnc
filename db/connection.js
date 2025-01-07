c9dadd0e6b72eddd7fba3e1f4c9dca434948393f
legshog <chloekelleher@hotmail.co.uk>
2025-01-07 18:52:45 +0000

    connection file debug

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
 const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "development";
-
-require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+const path = `${__dirname}/../.env.${ENV}`;
+require("dotenv").config({ path });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
@@ -13,10 +13,6 @@ const config = {};
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
-}
-
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE not set");
 }
 
 const db = new Pool(config);
