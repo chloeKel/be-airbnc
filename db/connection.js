f42ddba8a86ee99353b10a5ecdc3ba3e0a84c281
legshog <chloekelleher@hotmail.co.uk>
2025-02-23 17:55:39 +0000

    troubleshooting

--- db/connection.js
+++ db/connection.js
@@ -13,6 +13,9 @@ const config = {};
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
+  config.ssl = {
+    rejectUnauthorized: false,
+  };
   config.host = process.env.DATABASE_HOST || "0.0.0.0";
 }
 
