5fd16457f6f776f64d7e9da40023b9ad3e15ab45
legshog <chloekelleher@hotmail.co.uk>
2025-02-23 17:11:26 +0000

    troubleshooting

--- db/connection.js
+++ db/connection.js
@@ -13,6 +13,7 @@ const config = {};
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
+  config.host = process.env.DATABASE_HOST || "0.0.0.0";
 }
 
 const db = new Pool(config);
--- listen.js
+++ listen.js
@@ -1,4 +1,5 @@
 const app = require("./app/app");
+require("dotenv").config();
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
