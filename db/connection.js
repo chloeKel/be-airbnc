802500fbe0a364129a43734ebc63a91f5dbf7ea2
legshog <chloekelleher@hotmail.co.uk>
2025-03-11 17:32:21 +0000

    debugged properties query

--- db/connection.js
+++ db/connection.js
@@ -1,24 +1,13 @@
 const { Pool } = require("pg");
-
 const ENV = process.env.NODE_ENV || "development";
 const path = `${__dirname}/../.env.${ENV}`;
+
 require("dotenv").config({ path });
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
+if (!process.env.PGDATABASE) {
+  throw new Error("No PGDATABASE configured");
 }
 
-const config = {};
-
-if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-  config.ssl = {
-    rejectUnauthorized: false,
-  };
-  config.host = process.env.DATABASE_HOST || "0.0.0.0";
-}
-
-const db = new Pool(config);
+const db = new Pool();
 
 module.exports = db;
--- listen.js
+++ listen.js
@@ -2,4 +2,4 @@ const app = require("./app/app");
 require("dotenv").config();
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+app.listen(PORT);
